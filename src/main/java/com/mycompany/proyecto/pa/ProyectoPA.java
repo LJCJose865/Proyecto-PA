a5bccbf0ca125aca5e8f5e1a32e443a087e7a42f
Jose Perez <josedperez@ucompensar.edu.co>
2026-05-23 19:43:17 -0500

    feat: Inizalizacion de proyecto java

---
+++ src/main/java/com/mycompany/proyecto/pa/ProyectoPA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto.pa;
+
+/**
+ *
+ * @author josep
+ */
+public class ProyectoPA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
